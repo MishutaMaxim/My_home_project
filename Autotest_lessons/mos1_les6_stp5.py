8229f8e137b6950177a3b30c77fc6e62ff153fb7
mishuta <2Aol34mkv>
2021-10-27 09:05:21 +0300

    refactor code

--- Autotest_lessons/mos1_les6_stp5.py
+++ Autotest_lessons/mos1_les6_stp5.py
@@ -2,22 +2,24 @@ from selenium import webdriver
 import time
 import math
 
+from selenium.webdriver.common.by import By
+
 link = "http://suninjuly.github.io/find_link_text"
 
 try:
     browser = webdriver.Chrome()
     browser.get(link)
 
-    browser.find_element_by_partial_link_text(str(math.ceil(math.pow(math.pi, math.e)*10000))).click()
-    input1 = browser.find_element_by_name('first_name')
+    browser.find_element(By.PARTIAL_LINK_TEXT, str(math.ceil(math.pow(math.pi, math.e)*10000))).click()
+    input1 = browser.find_element(By.NAME, 'first_name')
     input1.send_keys("Ivan")
-    input2 = browser.find_element_by_name('last_name')
+    input2 = browser.find_element(By.NAME, 'last_name')
     input2.send_keys("Petrov")
-    input3 = browser.find_element_by_name('firstname')
+    input3 = browser.find_element(By.NAME, 'firstname')
     input3.send_keys("Smolensk")
-    input4 = browser.find_element_by_id('country')
+    input4 = browser.find_element(By.ID, 'country')
     input4.send_keys("Russia")
-    button = browser.find_element_by_css_selector("button.btn")
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
     button.click()
 
 finally:
