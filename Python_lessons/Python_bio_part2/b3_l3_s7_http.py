f2cc230afde8531fef6b6c97c570848b4c67a5cd
mishuta <2Aol34mkv>
2021-10-30 01:40:08 +0300

    regular expression

--- Python_lessons/Python_bio_part2/b3_l3_s7_http.py
+++ Python_lessons/Python_bio_part2/b3_l3_s7_http.py
@@ -1,6 +1,17 @@
 import requests
 import re
 
+# Получаем ссылку и скачиваем текст страницы
 link = input()
 text = requests.get(link).text
-links = re.findall(r'https:.+?html', text)
+# Паттерн для выборки ссылок
+pattern = r'<a.+href\=\"?\'?(?:\w+://)?(\w+(?:\.*\w+-*)+).+\"?\'?>'
+# Парсим все адреса сайтов в ссылках
+links = re.findall(pattern, text)
+# Удаляем дубли из списка
+links = list(set(links))
+# Сортируем список
+links.sort()
+# Выводим
+for element in links:
+    print(element)
