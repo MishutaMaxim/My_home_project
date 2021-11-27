b6f045d1d15bc12eb7b270e71dabe19ad08412f3
mishuta <2Aol34mkv>
2021-11-27 20:49:35 +0300

    you gonna work bitch

--- Autotest_lessons/sandbox.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-
-# инициализируем драйвер браузера. После этой команды вы должны увидеть новое открытое окно браузера
-driver = webdriver.Chrome()
-
-driver.get("https://stepik.org/lesson/25969/step/8")
-print(driver.current_url)
