fbf5be9ac96e95a56833690bd9bc81a91c2ea916
mishuta <2Aol34mkv>
2022-02-14 23:01:45 +0300

    Попытки сделать ДЗ 6

--- Python_lessons/Some_coding/LeetCode/121. Best Time to Buy and Sell Stock.py
+++ Python_lessons/Some_coding/LeetCode/121. Best Time to Buy and Sell Stock.py
@@ -1,6 +1,16 @@
 def maxProfit(prices) -> int:
+    bestbuy = prices[0]
+    bestsell = prices[0]
+    mprofit = 0
+    for el in prices:
+        profit = el - bestbuy
+        if profit > mprofit:
+            mprofit = profit
+        if el < bestbuy:
+            bestbuy = el
+        if el > bestsell:
+            bestsell = el
+    return mprofit
 
-
-a = [7,1,5,3,6,4]
-
+a = [7,12,1,5,3,6,11,4]
 print(maxProfit(a))
